82cc6b6ff8204c2f62190c110ee016613b84f2f9
Enoch <enoch.binas@gmail.com>
2024-07-28 19:34:12 +0800

    go init

---
+++ hello.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+
+	"rsc.io/quote"
+)
+
+const spanish = "Spanish"
+const french = "French"
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+
+func main() {
+	fmt.Println(quote.Opt())
+}
+
+func Hello(name, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	prefix := englishHelloPrefix
+
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	}
+
+	return prefix + name
+}
