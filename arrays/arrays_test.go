e75e7d90bd200b0050a669df919f5449bc8b7fdc
Enoch <enoch.binas@gmail.com>
2024-07-31 19:35:12 +0800

    arrays and slices

--- arrays/arrays_test.go
+++ arrays/arrays_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 	"testing"
 )
 
@@ -43,4 +44,57 @@ func ExampleSum() {
 	total := Sum(numbers)
 	fmt.Println(total)
 	// Output: 15
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
+}
+
+func TestSumAllTails(t *testing.T) {
+	t.Run("Slices with tails", func(t *testing.T) {
+		got := SumAllTails([]int{1, 4, 5}, []int{0, 9})
+		want := []int{9, 9}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	})
+
+	t.Run("Slices without tails, but called.", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{10})
+		want := []int{0, 0}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("Got %v, want %v", got, want)
+		}
+	})
+
+}
+
+func SumAllTails(numbers ...[]int) []int {
+	var sums []int
+	for _, nums := range numbers {
+		if len(nums) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+
+	return sums
 }
